5612eb1206f31026e2049d8c5cafed8c2e630fbd
illus0r <zor667@gmail.com>
2019-10-07 15:12:16 +0400

    ¯\_(ツ)_/¯

--- dist/floats.e31bb0bc.js
+++ dist/floats.e31bb0bc.js
@@ -118,7 +118,37 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"index.js":[function(require,module,exports) {
-console.log('Привет, Мир!');
+var partsAvailable = [];
+
+function init() {
+  // Load external SVG file
+  // Get the list of float's parts
+  // temporary placeholder
+  partsAvailable = [[256, 256, 120], [256, 256, 120], [16, 16, 40], [16, 256, 100], [256, 16, 40], [16, 16, 40], [16, 256, 100], [256, 16, 40]]; // Set function onTextChange() to run on every text change. And run it for the first time.
+  // Launch the animation
+
+  animate();
+} // Ivan's part, which determines the subset of parts and their order
+
+
+function composer(partsAvailable_) {
+  var inputText = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : "Hello";
+  // temporary placeholder
+  var parts = partsAvailable_.slice(0, 3);
+  return parts;
+}
+
+function onTextChange() {
+  // Call Ivan's composer
+  var parts = composer(partsAvailable, inputText); // Draw parts in proper order on proper places
+  // Scale the float to fit the screen
+} // Runs infinitely
+
+
+function animate() {// move SVG's group
+}
+
+init();
 },{}],"../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-console.log('Привет, Мир!')
+var partsAvailable = [										 ]
+function init(){
+	// Load external SVG file
+
+	// Get the list of float's parts
+	// temporary placeholder
+	partsAvailable = [
+											[256, 256, 120],
+											[256, 256, 120],
+											[ 16,  16,  40],
+											[ 16, 256, 100],
+											[256,  16,  40],
+											[ 16,  16,  40],
+											[ 16, 256, 100],
+											[256,  16,  40],
+	]
+
+	// Set function onTextChange() to run on every text change. And run it for the first time.
+
+	// Launch the animation
+	animate()
+}
+
+// Ivan's part, which determines the subset of parts and their order
+function composer(partsAvailable_, inputText="Hello"){
+	// temporary placeholder
+	let parts = partsAvailable_.slice(0, 3)
+	return parts
+}
+
+function onTextChange(){
+	// Call Ivan's composer
+	let parts = composer(partsAvailable, inputText)
+
+	// Draw parts in proper order on proper places
+
+	// Scale the float to fit the screen
+}
+
+// Runs infinitely
+function animate(){
+	// move SVG's group
+}
+
+init();
