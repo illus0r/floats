0662e55b5c1edaef8140bb09f0b7763a95ef6a5e
illus0r <zor667@gmail.com>
2019-10-07 15:23:56 +0400

    ¯\_(ツ)_/¯

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-var partsAvailable = [										 ]
+var partsAvailable = []
 function init(){
 	// Load external SVG file
 
 	// Get the list of float's parts
 	// temporary placeholder
 	partsAvailable = [
-											[256, 256, 120],
-											[256, 256, 120],
-											[ 16,  16,  40],
-											[ 16, 256, 100],
-											[256,  16,  40],
-											[ 16,  16,  40],
-											[ 16, 256, 100],
-											[256,  16,  40],
+		[256, 256, 120],
+		[256, 256, 120],
+		[ 16,  16,  40],
+		[ 16, 256, 100],
+		[256,  16,  40],
+		[ 16,  16,  40],
+		[ 16, 256, 100],
+		[256,  16,  40],
 	]
 
 	// Set function onTextChange() to run on every text change. And run it for the first time.
